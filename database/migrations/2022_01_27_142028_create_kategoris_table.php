e5b4804a9fdf8510adc745cbf4a8cac8b20fd439
Miftahul Haq <ciftah12@gmail.com>
2022-02-01 00:47:55 +0700

    tutorial 8

--- database/migrations/2022_01_27_142028_create_kategoris_table.php
+++ database/migrations/2022_01_27_142028_create_kategoris_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateKategorisTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-
-        Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('post_id')->from(887);
-            $table->foreignId('kateg_id');
-            $table->string('title');
+        Schema::create('kategoris', function (Blueprint $table) {
+            $table->bigIncrements('kateg_id')->from(54);
+            $table->string('nama')->unique();
             $table->string('slug')->unique();
-            $table->text('excerpt');
-            $table->text('body');
-            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('kategoris');
     }
 }
